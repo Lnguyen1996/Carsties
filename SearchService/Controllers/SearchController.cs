9e137a9ab0265dc1a79902fcfe9bf8210ff92c27
Lam Nguyen <97846628+Lnguyen1996@users.noreply.github.com>
2024-07-24 21:56:07 -0400

    add pagination

--- SearchService/Controllers/SearchController.cs
+++ SearchService/Controllers/SearchController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
 
@@ -10,19 +9,30 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
         {
-            var query = DB.Find<Item>();
+            var query = DB.PagedSearch<Item>();
 
             if (!string.IsNullOrEmpty(searchTerm))
             {
                 query.Match(Search.Full, searchTerm).SortByTextScore();
             }
+
+            query.PageNumber(pageNumber);
+
+            query.PageSize(pageSize);
+
+
             query.Sort(x => x.Ascending(a => a.Make));
 
             var results = await query.ExecuteAsync();
 
-            return results;
+            return Ok(new
+            {
+                results = results.Results,
+                pageCount = results.PageCount,
+                totalCount = results.TotalCount
+            });
         }
     }
 }
