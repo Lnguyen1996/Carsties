25fea83e57fcb7be302b41d417e713e91d33ebaa
lnguyen4e@gmail.com <lnguyen4e@gmail.com>
2024-11-02 18:34:22 -0400

    Gateway added

---
+++ SearchService/Comsumers/BidPlacedConsumer.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace SearchService.Comsumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        public Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
